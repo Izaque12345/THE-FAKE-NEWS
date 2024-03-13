8630c45880a65a532ab074aa2110ba4f5d5dcc0a
Izaque12345 <andradeizaque01@Gmail.com>
2024-03-13 16:29:39 -0300

    Correção de erros na página Apresentacao.html Mudança do nome do segundo link da página Index.html Biblioteca Adicionada Página de ERROR Adicionada Detalhes de atualização adicionada na versão da página

---
+++ docs/Biblioteca/Biblioteca!!.js
@@ -0,0 +1,45 @@
+function toggleCSS() {
+  var screenWidth = window.innerWidth;
+  var cssLink = document.getElementById('css-link');
+  var tituloT = document.getElementById("tituloT");
+
+  if (screenWidth <= 768) {
+    cssLink.href = 'BibliotecaCELL!!.css';
+    tituloT.style.textAlign = "left"
+    console.log("Mobile");
+  } else {
+    cssLink.href = 'BibliotecaPC!!.css';
+    tituloT.style.textAlign = "left"
+    console.log("Desktop");
+  }
+}
+
+// Chamar a função quando a página carregar e quando a tela for redimensionada
+window.addEventListener('load', toggleCSS);
+window.addEventListener('resize', toggleCSS);
+
+// Redirecionar para as páginas
+var links = [
+  'Fake News/NoticiasFalsas.html',
+  '/docs/Page Error/MSGerro.html']
+  
+  function Redirect() {
+    window.location.href = `${links[1]}`
+  }
+
+// Back Page
+function BackPage() {
+window.history.back();
+}
+
+window.addEventListener('scroll', function() {
+var button = document.getElementById('button-home');
+var scrollPosition = window.scrollY + window.innerHeight;
+var documentHeight = document.body.scrollHeight;
+
+if (scrollPosition >= documentHeight) {
+button.style.opacity = '0.5'; // 50% transparente
+} else {
+button.style.opacity = '1'; // Opacidade normal
+}
+});
